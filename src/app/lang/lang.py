eba723d471b7787cd21650c532e8ab977825e8d5
Евгений <aev@smartgroup.pro>
2023-11-07 11:14:20 +0300

    update

--- src/app/lang/lang.py
+++ src/app/lang/lang.py
@@ -1,27 +1,33 @@
 import os
+import sys
 import logging
 import xml.etree.ElementTree as et
 
 from app.config.settings import _config
 
+
 class Lang(object):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, base_path=None):
         try:
-            file_path = os.path.abspath('locales/{}.xml'.format(_config.lang))
+            main_script_path = sys.argv[0]
+            main_script_dir = os.path.dirname(
+                os.path.abspath(main_script_path))
+
+            file_path = os.path.join(
+                main_script_dir, 'locales', '{}.xml'.format(_config.lang))
+            # file_path = os.path.abspath('locales/{}.xml'.format(_config.lang))
             logging.info('Load lang "{}"'.format(_config.lang))
             self.tree = et.parse(file_path).getroot()
         except:
-            logging.info('Fail load lang "{}", load default "ru-RU"'.format(_config.lang))
-            file_path = os.path.abspath('locales/ru-RU.xml'.format(_config.lang))
+            logging.info(
+                'Fail load lang "{}", load default "ru-RU"'.format(_config.lang))
+            file_path = os.path.abspath(
+                'locales/ru-RU.xml'.format(_config.lang))
             self.tree = et.parse(file_path).getroot()
 
-        
     def get(self, key):
         text = self.tree.find('.//String[@key="{}"]'.format(key)).text
         if text is None:
             return '{}(Not found text)'.format(key)
         return text
-        
-
-
--- src/app/tgcli/cli_worker.py
+++ src/app/tgcli/cli_worker.py
@@ -17,32 +17,47 @@ from app.tgcli.help import *
 from app.tgcli.exeptions import NotFoundTGAccountException
 
 
-
 class TGCli(object):
 
     def __init__(self, *args, **kwargs):
-        self.cliapp = Client(_config.cliSession, _config.cliApiId, _config.cliApiHash)
-        self.cliapp.start()    
+        self.cliapp = Client(_config.cliSession,
+                             _config.cliApiId, _config.cliApiHash)
+        self.cliapp.start()
         self.reg_handler()
-        self.CONTACTS = self.cliapp.get_contacts()
+        self.CONTACTS = []
+        self.init_contacts()
 
     def reg_handler(self):
-        self.cliapp.add_handler(MessageHandler(self.who_am_i, filters.command(_command.get_command_name('me'), _command.get_command_pref('me'))))
-        self.cliapp.add_handler(MessageHandler(service_state, filters.command(_command.get_command_name('state'), _command.get_command_pref('state'))))
-        self.cliapp.add_handler(MessageHandler(prometheus_state, filters.command(_command.get_command_name('prom'), _command.get_command_pref('prom'))))
-        self.cliapp.add_handler(MessageHandler(alerts_state, filters.command(_command.get_command_name('alerts'), _command.get_command_pref('alerts'))))
-        self.cliapp.add_handler(MessageHandler(metric_state, filters.command(_command.get_command_name('metric'), _command.get_command_pref('metric'))))
-        self.cliapp.add_handler(MessageHandler(help, filters.command(_command.get_command_name('help'), _command.get_command_pref('help'))))
-        self.cliapp.add_handler(MessageHandler(man, filters.command(_command.get_command_name('man'), _command.get_command_pref('man'))))
+        self.cliapp.add_handler(MessageHandler(self.who_am_i, filters.command(
+            _command.get_command_name('me'), _command.get_command_pref('me'))))
+        self.cliapp.add_handler(MessageHandler(service_state, filters.command(
+            _command.get_command_name('state'), _command.get_command_pref('state'))))
+        self.cliapp.add_handler(MessageHandler(prometheus_state, filters.command(
+            _command.get_command_name('prom'), _command.get_command_pref('prom'))))
+        self.cliapp.add_handler(MessageHandler(alerts_state, filters.command(
+            _command.get_command_name('alerts'), _command.get_command_pref('alerts'))))
+        self.cliapp.add_handler(MessageHandler(metric_state, filters.command(
+            _command.get_command_name('metric'), _command.get_command_pref('metric'))))
+        self.cliapp.add_handler(MessageHandler(help, filters.command(
+            _command.get_command_name('help'), _command.get_command_pref('help'))))
+        self.cliapp.add_handler(MessageHandler(man, filters.command(
+            _command.get_command_name('man'), _command.get_command_pref('man'))))
 
     def stop(self):
         self.cliapp.stop()
 
+    def init_contacts(self):
+        temp = self.cliapp.get_contacts()
+        for contact in temp:
+            if contact.phone_number != None and contact.phone_number not in self.CONTACTS:
+                self.CONTACTS.append(contact)
+        return self.CONTACTS
+
     def who_am_i(self, client, message):
         logging.info('Recieve command "me" message from user({0}) Phone:{1} Name: {3} {2} Username:{4}'.format(message.from_user.id, message.from_user.phone_number,
-                    message.from_user.last_name, message.from_user.first_name, message.from_user.username))
-        client.send_message(message.from_user.id, 'Ваш ID: {}'.format(message.from_user.id))
-
+                                                                                                               message.from_user.last_name, message.from_user.first_name, message.from_user.username))
+        client.send_message(message.from_user.id,
+                            'Ваш ID: {}'.format(message.from_user.id))
 
     async def sendMsg(self, phone, msg):
         try:
@@ -53,20 +68,6 @@ class TGCli(object):
             self.sendMsg(phone, msg)
 
     async def get_or_create_contact(self, phone):
-        phone_numbers = [contact.phone_number for contact in self.CONTACTS]
-        if phone_numbers and any(number in phone for number in phone_numbers):
-            return next(contact for contact in self.CONTACTS if contact.phone_number in phone)
-
-        contact = await self.cliapp.import_contacts([InputPhoneContact(phone, phone)])
-        if contact.users:
-            new_contact = contact.users[0]
-            self.CONTACTS.append(new_contact)
-            return new_contact
-        else:
-            raise NotFoundTGAccountException(f"No found telegramm account. Phone {phone}")
-
-
-    async def get_or_create_contact(self, phone):
         for contact in self.CONTACTS:
             if contact.phone_number in phone:
                 return contact
@@ -75,10 +76,8 @@ class TGCli(object):
             self.CONTACTS.append(contact.users[0])
             return contact.users[0]
         else:
-            raise NotFoundTGAccountException("No found telegramm account. Phone {}".format(phone))
-
-
-
+            raise NotFoundTGAccountException(
+                "No found telegramm account. Phone {}".format(phone))
 
 
 cliWorker = TGCli()
